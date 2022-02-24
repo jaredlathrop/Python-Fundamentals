eace9f08e52f1b5f26cda0d314f86b01a20c266b
jlathrop <jared.lathrop44@gmail.com>
2022-02-24 11:22:00 -0600

    CAC-102 Created Python Project

---
+++ hello.py
@@ -0,0 +1,10 @@
+# This is a comment and you should have space after the number sign
+# click the enter key after the comment to remove the squiggly line
+
+# the statement below is a top level statement
+# it is defined outside of a function or class
+# this specific statement is a function called print that takes information and displays that information to the terminal
+print("Hello World")
+
+# a basic string can be defined using either single or double quotes
+print('Hello Again!')
