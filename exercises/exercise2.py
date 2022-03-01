c79292ebe10775366d546d72298861034f6c5f2b
jlathrop <jared.lathrop44@gmail.com>
2022-03-01 11:00:01 -0600

    CAC 102-2 My Application Overview Lab

---
+++ exercises/exercise2.py
@@ -0,0 +1,75 @@
+# This is for problem 1.
+message_1 = "message 1"
+message_2 = "message 2"
+message_3 = "message 3"
+message_4 = "message 4"
+message_5 = "message 5"
+
+
+def simple():
+    print(message_1)
+    print(message_2)
+    print(message_3)
+    print(message_4)
+    print(message_5)
+
+
+simple()
+
+# This is for problem 2
+
+message_6 = "This is the 6th message"
+
+
+def simple2():
+    print(message_6)
+
+
+simple2()
+message_6 = "This is the 6th message but changed"
+
+
+def simple2():
+    print(message_6)
+
+
+simple2()
+message_6 = "This is the 6th message but changed again"
+
+
+def simple2():
+    print(message_6)
+
+
+simple2()
+message_6 = "This is the 6th message changed again for the 3rd time"
+
+
+def simple2():
+    print(message_6)
+
+
+simple2()
+
+
+# This is for problem 3. I didn't read through the entire assignment and used message for all of my stuff, so I will be
+# using the word communication instead.
+def communication(arg1):
+    print(arg1)
+
+
+communication("This is my argument for problem 3")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
