0cad980f425176893388bc29abbf89421e263ef4
jlathrop <jared.lathrop44@gmail.com>
2022-03-08 11:10:02 -0600

    CAC 102-7 Exercise 7 Loops

---
+++ exercises/exercise7.py
@@ -0,0 +1,44 @@
+# problem 1
+# count to 20
+def problem_1():
+    for nums1 in range(1, 21):
+        print(nums1)
+
+
+problem_1()
+
+
+# Problem 2
+# making a list of odd numbers.
+def problem_2():
+    for nums2 in range(1, 21, 2):
+        print(nums2)
+
+
+problem_2()
+
+
+# Problem 3
+# List of the multiples of 3 from 3 to 30.
+def problem_3():
+    for nums3 in range(3, 31, 3):
+        print(nums3)
+
+
+problem_3()
+
+
+# Problem 4
+def problem_4():
+    cubes = []
+    for value in range(1, 11):
+        cube = value ** 3
+        cubes.append(cube)
+    print(cubes)
+
+
+problem_4()
+
+
+
+
